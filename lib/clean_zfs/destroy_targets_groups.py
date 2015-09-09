e5575c15c5b1bdbe019a1ebd0c184b95e07f7bcd
Sun Ning <sun.ning@oracle.com>
2015-09-09 14:27:21 +0800

    update ZFS cleanup script

--- lib/clean_zfs/destroy_targets_groups.py
+++ lib/clean_zfs/destroy_targets_groups.py
@@ -8,7 +8,7 @@ import sys
 zfs_server="slce39sn01.us.oracle.com"
 port=22
 user="root"
-passwd="welcome1"
+passwd="J9K2vcmr"
 target_cmd="confirm configuration san iscsi targets destroy "
 group_cmd="confirm configuration san iscsi targets groups destroy "
 
@@ -16,29 +16,30 @@ def inred(str):
     return"%s[1;31m%s%s[0m"%(chr(27), str, chr(27))
 
 targets_file = open("tmp/targets")
-#groups_file = open("tmp/groups")
+groups_file = open("tmp/groups")
 
 ssh=paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 #ssh.connect(hostname=host,username=user,password=passwd)
 ssh.connect(hostname=zfs_server,port=port,username=user,password=passwd)
 
-
-while 1:
-    lines = targets_file.readlines(255)
-    if not lines:
-        break
-    for line in lines:
-        print "###### Start to destory targets %s" %line
-        print "%s%s\n" %(target_cmd,line)
-        stdin,stdout,stderr = ssh.exec_command("%s%s\n" %(target_cmd,line))
-
+## Destroy the targets
 #while 1:
-#    lines = groups_file.readlines(255)
+#    lines = targets_file.readlines(255)
 #    if not lines:
 #        break
 #    for line in lines:
-#        print "###### Start to destory groups %s\n" %line
-#        print "%s%s\n" %(group_cmd,line)
-#        stdin,stdout,stderr = ssh.exec_command("%s%s\n" %(group_cmd,line))
-#        print "%s\n" %(inred(stdout.read()))
+#        print "###### Start to destory targets %s" %line
+#        print "%s%s\n" %(target_cmd,line)
+#        stdin,stdout,stderr = ssh.exec_command("%s%s\n" %(target_cmd,line))
+
+# Destroy the groups
+while 1:
+    lines = groups_file.readlines(255)
+    if not lines:
+        break
+    for line in lines:
+        print "###### Start to destory groups %s\n" %line
+        print "%s%s\n" %(group_cmd,line)
+        stdin,stdout,stderr = ssh.exec_command("%s%s\n" %(group_cmd,line))
+        print "%s\n" %(inred(stdout.read()))
